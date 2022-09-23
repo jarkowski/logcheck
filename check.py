301398f05574d29736be912bdea12287d522a232
David <david@jarkowski.name>
2022-09-23 18:53:20 +0200

    ok

--- check.py
+++ check.py
@@ -2,17 +2,17 @@
 # ToDo:  Run report on login automatically
 
 
-def search(filename, text):
+def search(filename, grepsting, datestart, dateend, leftsplit, rightsplit):
     with open(filename) as f:
         for line in f:
-            if text in line:
-                print(line[7:17])
-                split1 = line.split("ChatMember[")
+            if grepsting in line:
+                datecode = line[datestart:dateend]
+                split1 = line.split(leftsplit)
                 split2 = split1[1]
-                split3 = split2.split("@conference.meeting")
+                split3 = split2.split(rightsplit)
                 split4 = split3[0]
-                print(split4)
+                print(f"{datecode} UTC - user on session: {split4}")
 
 
 if __name__ == "__main__":
-    search("log.log", "Electing")
+    search("log.log", "Electing", 7, 23, "ChatMember[", "@conference.meeting")
