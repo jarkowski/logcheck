eed6fdaea90126368ebaf06c0f79a901b3999d69
David <david@jarkowski.name>
2022-09-23 18:39:44 +0200

    split ok

--- check.py
+++ check.py
@@ -6,7 +6,12 @@ def search(filename, text):
     with open(filename) as f:
         for line in f:
             if text in line:
-                print(line)
+                print(line[7:17])
+                split1 = line.split("ChatMember[")
+                split2 = split1[1]
+                split3 = split2.split("@conference.meeting")
+                split4 = split3[0]
+                print(split4)
 
 
 if __name__ == "__main__":
